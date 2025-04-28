3b5a7e7966c166c928eda016c581ee75d7608b91
Squid1003 <jeffrey314271@gmail.com>
2025-04-28 21:34:44 +0000

    Fixes to ai_target component

--- app/engine/item_components/ai_components.py
+++ app/engine/item_components/ai_components.py
@@ -8,9 +8,10 @@ class NoAI(ItemComponent):
     def ai_priority(self, unit, item, target, move):
         return -1
 
-class TargetAI(ItemComponent):
-    nid = 'targetai'
-    desc = "Gives a condition under which the AI will target units with this item.  Higher priority makes the AI more likely to use the item."
+class EvalAIPriority(ItemComponent):
+    nid = 'eval_ai_priority'
+    desc = """Gives a condition under which the AI will target units with this item.  Higher priority makes the AI more likely to use this item.
+    Priority values should typically be between 0 and 1, but values above 1 can be used to more strongly encourage an action."""
     tag = ItemTags.UTILITY
     expose = ComponentType.NewMultipleOptions
     options = {
@@ -30,10 +31,15 @@ class TargetAI(ItemComponent):
         if target:
             from app.engine import evaluate
             try:
-                if evaluate.evaluate(self.value['condition'], unit, target, local_args={'item': item}):
-                    priority_term = evaluate.evaluate(self.value['priority'], unit, target, local_args={'item': item})
-                    return priority_term
-            except:
-                print("Could not evaluate condition or priority.")
+                condition: bool = bool(evaluate.evaluate(self.value['condition'], unit, target, local_args={'item': item, 'move': move}))
+            except Exception as e:
+                logging.error("eval_ai_priority failed to evaluate condition %s with error %s", self.value['condition'], e)
+                return 0
+            if condition:
+                    try:
+                        priority_term: float = float(evaluate.evaluate(self.value['priority'], unit, target, local_args={'item': item, 'move': move}))
+                        return priority_term
+                    except Exception as e:
+                        logging.error("eval_ai_priority failed to evaluate priority %s with error %s", self.value['priority'], e)
         return 0
     
