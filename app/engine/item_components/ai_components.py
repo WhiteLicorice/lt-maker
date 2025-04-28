42ba4f649325d9e9e02d285625574222f8277640
Squid1003 <jeffrey314271@gmail.com>
2025-04-28 01:56:43 +0000

    Add target_ai item component.

--- app/engine/item_components/ai_components.py
+++ app/engine/item_components/ai_components.py
@@ -7,4 +7,33 @@ class NoAI(ItemComponent):
 
     def ai_priority(self, unit, item, target, move):
         return -1
+
+class TargetAI(ItemComponent):
+    nid = 'targetai'
+    desc = "Gives a condition under which the AI will target units with this item.  Higher priority makes the AI more likely to use the item."
+    tag = ItemTags.UTILITY
+    expose = ComponentType.NewMultipleOptions
+    options = {
+        'condition': ComponentType.String,
+        'priority': ComponentType.String
+    }
+
+    def __init__(self, value=None):
+        self.value = {
+            'condition': 'True',
+            'priority': '0'
+        }
+        if value:
+            self.value.update(value)
+    
+    def ai_priority(self, unit, item, target, move):
+        if target:
+            from app.engine import evaluate
+            try:
+                if evaluate.evaluate(self.value['condition'], unit, target, local_args={'item': item}):
+                    priority_term = evaluate.evaluate(self.value['priority'], unit, target, local_args={'item': item})
+                    return priority_term
+            except:
+                print("Could not evaluate condition or priority.")
+        return 0
     
